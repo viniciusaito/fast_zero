a1a99b39bdfd3a857a4f27b44c5a14db94826c13
Vinicius Saito <viniciusaitosilva@gmail.com>
2024-06-13 22:21:25 -0300

    Fixing test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,14 @@
 from http import HTTPStatus
 
-from fastapi import testclient
+from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
 
-def test_read_root_deve_retornar_ok_e_ola_bb():
-    client = testclient(app)  # Arrange (organização)
+def test_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)  # Arrange
 
-    response = client.get('/')  # Act (ação)
+    response = client.get('/')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
-    assert response.json == {'message': 'Olá bb!'}
+    assert response.json() == {'message': 'Olá bb!'}  # Assert
