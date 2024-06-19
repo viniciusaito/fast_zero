e1bc22aff34d3a9ec20777a7996f71f5febe1f5d
Vinicius Saito <viniciusaitosilva@gmail.com>
2024-06-18 22:03:48 -0300

    Adding a new html response + tests

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
-
 from http import HTTPStatus
+
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from fast_zero.schemas import Message
 
@@ -10,3 +11,17 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá bb!'}
+
+
+@app.get('/olamundo', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def ola_mundo():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,21 @@ def test_root_should_return_root_and_ola_bb():
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {'message': 'Olá bb!'}  # Assert
+
+
+def test_olamundo_should_return_html_with_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/olamundo')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>
+    """
