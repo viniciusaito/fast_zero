83d372071f88f4b6aa2eada33f2d4d051e9614d1
Vinicius Saito <viniciusaitosilva@gmail.com>
2024-06-20 22:09:55 -0300

    Adding tests for put and delete 404 not found

--- tests/test_app.py
+++ tests/test_app.py
@@ -59,24 +59,44 @@ def test_read_users(client):
 
 
 def test_update_user(client):
+    test_user = {
+        'username': 'testusername',
+        'email': 'test@test.com',
+        'password': 'password',
+    }
     response = client.put(
         '/users/1',
-        json={
-            'username': 'testusername',
-            'email': 'test@test.com',
-            'password': 'password',
-        },
+        json=test_user,
     )
+    response_404_user_above_max = client.put(
+        '/users/2',
+        json=test_user,
+    )
+    response_404_user_below_min = client.put(
+        '/users/0',
+        json=test_user,
+    )
+
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
         'username': 'testusername',
         'email': 'test@test.com',
         'id': 1,
     }
+    assert response_404_user_above_max.status_code == HTTPStatus.NOT_FOUND
+    assert response_404_user_above_max.json() == {'detail': 'User not found'}
+    assert response_404_user_below_min.status_code == HTTPStatus.NOT_FOUND
+    assert response_404_user_below_min.json() == {'detail': 'User not found'}
 
 
 def test_delete_user(client):
     response = client.delete('/users/1')
+    response_404_user_above_max = client.delete('/users/2')
+    response_404_user_below_min = client.delete('/users/0')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
+    assert response_404_user_above_max.status_code == HTTPStatus.NOT_FOUND
+    assert response_404_user_above_max.json() == {'detail': 'User not found'}
+    assert response_404_user_below_min.status_code == HTTPStatus.NOT_FOUND
+    assert response_404_user_below_min.json() == {'detail': 'User not found'}
