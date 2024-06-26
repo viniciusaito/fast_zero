5b3455c3c3ed70d0852da83fa2c79d44f1c9d571
Vinicius Saito <viniciusaitosilva@gmail.com>
2024-06-25 22:44:09 -0300

    Alterando modelo e syncando migrações

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -18,5 +18,7 @@ class User:
         init=False, server_default=func.now()
     )
     updated_at: Mapped[datetime] = mapped_column(
-        init=False, onupdate=func.now(), nullable=True
+        init=False,
+        server_default=func.now(),
+        onupdate=func.current_timestamp(),
     )
