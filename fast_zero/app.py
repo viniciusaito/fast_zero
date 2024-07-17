cffa5006b30e5ff32d6490d579ee660907d66672
Vinicius Saito <viniciusaitosilva@gmail.com>
2024-07-16 21:51:06 -0300

    Implementado os endpoints de tarefas

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -3,13 +3,14 @@ from http import HTTPStatus
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-from fast_zero.routers import auth, users
+from fast_zero.routers import auth, todos, users
 from fast_zero.schemas import Message
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(todos.router)
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
--- fast_zero/models.py
+++ fast_zero/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+from enum import Enum
 
-from sqlalchemy import func
+from sqlalchemy import ForeignKey, func
 from sqlalchemy.orm import Mapped, mapped_column, registry
 
 table_registry = registry()
@@ -20,3 +21,22 @@ class User:
     updated_at: Mapped[datetime] = mapped_column(
         init=False, default=func.now(), onupdate=func.now(), nullable=True
     )
+
+
+class TodoState(str, Enum):
+    draft = 'draft'
+    todo = 'todo'
+    doing = 'doing'
+    done = 'done'
+    trash = 'trash'
+
+
+@table_registry.mapped_as_dataclass
+class Todo:
+    __tablename__ = 'todos'
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    state: Mapped[TodoState]
+    # Toda tarefa pertence a alguém
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
