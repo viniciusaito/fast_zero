f65a8b7ac41a1c0d885b7b5b2a7f2f72bbb911cf
viniciusaito <viniciusaitosilva@gmail.com>
2025-11-11 19:27:41 -0300

    Applying ruff formatting

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,24 +1,27 @@
-from unittest.mock import MagicMock
-from sqlalchemy.exc import SQLAlchemyError
 from http import HTTPStatus
+from unittest.mock import MagicMock
+
+from sqlalchemy.exc import SQLAlchemyError
+
 
 def test_health_check_ok(client):
-    response = client.get("/health")
+    response = client.get('/health')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        "app_status": "ok",
-        "database_status": "ok",
-        "timestamp": response.json()["timestamp"],
+        'app_status': 'ok',
+        'database_status': 'ok',
+        'timestamp': response.json()['timestamp'],
     }
+
 
 def test_health_check_error(client, session):
     session.execute = MagicMock(side_effect=SQLAlchemyError)
-    response = client.get("/health")
+    response = client.get('/health')
 
     assert response.status_code == HTTPStatus.SERVICE_UNAVAILABLE
     assert response.json() == {
-        "app_status": "error",
-        "database_status": "error",
-        "timestamp": response.json()["timestamp"],
+        'app_status': 'error',
+        'database_status': 'error',
+        'timestamp': response.json()['timestamp'],
     }
